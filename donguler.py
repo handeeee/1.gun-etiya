55cb4ded3f7c14bbfe5eb9c797a7cfa667ee7e26
handeeee <92745889+handeeee@users.noreply.github.com>
2023-04-06 10:47:08 +0300

    init

---
+++ donguler.py
@@ -0,0 +1,24 @@
+
+print("ilk sayfa")
+krediler=["hızlı kredi","halkbanktan maaşını alanlara özel","mutlu emekli "]
+#alias 
+for kredi in krediler:
+    print("<option>"+kredi+"<option>")
+
+print("ikinci sayfa")
+krediler=["hızlı kredi","halkbanktan maaşını alanlara özel","mutlu emekli "]
+#alias 
+for kredi in krediler:
+    print("<option>"+kredi+"<option>")
+
+
+print("üçüncü sayfa")
+krediler=["hızlı kredi","halkbanktan maaşını alanlara özel","mutlu emekli "]
+#alias 
+for kredi in krediler:
+    print("<option>"+kredi+"<option>")
+#def =>fonksiyon (definition), indententiona dikkat!
+def kredileriListele():
+    krediler=["hızlı kredi","halkbanktan maaşını alanlara özel","mutlu emekli "]
+    for kredi in krediler:
+        print("<option>"+kredi+"<option>")
---
+++ sartBloklari.py
@@ -0,0 +1,11 @@
+dolarDun=7.65
+dolarBugun=7.75
+
+if dolarDun>dolarBugun:
+    print("azalış oku")
+elif dolarDun<dolarBugun:
+    print("artiş oku")
+else:
+    print("eşittir oku")
+#else bloğuna dahil olmaması için bittiye indentation vermedik
+print("bitti")
